03ad999929775a96c2ddacbe3a78ab2f5a8f8bd4
Oluwatosin Oloruntoba <oluwatosin.oloruntoba@softescu.com>
2024-02-17 15:03:37 +0100

    Acme dashboard using next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export const metadata = {
+  title: "Acme Dashoard App",
+  description: "Tosin's first Next.js app",
+};
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
